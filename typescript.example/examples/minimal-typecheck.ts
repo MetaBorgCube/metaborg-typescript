f5ebf62824e1def0052bc8d4b8b153eb787c4212
Thomas Smith <txsmith@users.noreply.github.com>
2017-06-16 15:37:49 +0200

    Produce an error when a constant is not initilized (#15)
    * Produce an error when a constant is not initilized
    
    * Fix feedback
    
    * Fix example
    
    * Comment test, nabl2 does not support it
    
    * Move relations to init scope

--- typescript.example/examples/minimal-typecheck.ts
+++ typescript.example/examples/minimal-typecheck.ts
@@ -1,3 +1,15 @@
-var a: string = undefined;
+var a;
+
+function f(x: string) {
+  return x;
+}
+
+f(a);
+
+interface X {}
+
+const b: X = a;
+//const c: any = b;
+var c: string = undefined;
 //const b: A = { foo: 5 }
 //const c: A = 512
