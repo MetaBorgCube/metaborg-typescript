37d7860e73d61245424a19a768d3c35c288a4c27
Tim van der Lippe <tvanderlippe@gmail.com>
2017-06-16 14:35:34 +0200

    Comment file

--- typescript.example/examples/printAst.ts
+++ typescript.example/examples/printAst.ts
@@ -1,14 +1,15 @@
-filenname:"printAst"
-import {readFileSync} from "fs";
- import * as ts from 'typescript';
+filename:"printAst"
 
- const fileNames = process.argv.slice(2);
- fileNames.forEach(function(fileName) {
-     // Parse a file
-     let sourceFile = ts.createSourceFile(fileName, readFileSync(fileName).toString(), ts.ScriptTarget.ES2016, /*setParentNodes */ true);
-
-     sourceFile.statements.forEach(function(statement) {
-       console.log(statement);
-     })
- });
+//import {readFileSync} from "fs";
+//import * as ts from 'typescript';
+//
+// const fileNames = process.argv.slice(2);
+// fileNames.forEach(function(fileName) {
+//     // Parse a file
+//     let sourceFile = ts.createSourceFile(fileName, readFileSync(fileName).toString(), ts.ScriptTarget.ES2016, /*setParentNodes */ true);
+//
+//     sourceFile.statements.forEach(function(statement) {
+//       console.log(statement);
+//     })
+// });
 
