bcf0034f3397646e219a7d488988017fb03f5bc4
Thomas Smith <tnsmith@live.nl>
2017-06-16 13:53:57 +0200

    Make types for null and undefined

--- typescript.example/examples/minimal-typecheck.ts
+++ typescript.example/examples/minimal-typecheck.ts
@@ -1,14 +1,3 @@
-var a;
-
-function f(x: string) {
-	return x;
-}
-
-f(a);
-
-interface X {}
-
-const b: X = a;
-const c: any = b;
+var a: string = undefined;
 //const b: A = { foo: 5 }
 //const c: A = 512
